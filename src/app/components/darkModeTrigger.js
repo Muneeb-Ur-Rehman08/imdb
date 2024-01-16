8af8f17753707a224f7cb0abea82a15e547049da
Muneeb Rehman <117652966+MuneebRehman8@users.noreply.github.com>
2024-01-16 22:19:13 +0500

    formate documents using prettier

--- src/app/components/darkModeTrigger.js
+++ src/app/components/darkModeTrigger.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { MdLightMode } from "react-icons/md";
 import { BsMoonFill } from "react-icons/bs";
@@ -6,18 +6,25 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export function DarkModeSwitch() {
-    const { systemTheme, theme, setTheme } = useTheme();
-    const [mounted, setMounted] = useState(false)
-    useEffect(() => {
-        setMounted(true)
-    }, [])
-    const currentTheme = theme == 'system' ? systemTheme : theme;
-    return (
-        <div>
-            {mounted && theme == 'dark' ?
-                <MdLightMode className="text-xl hover:text-amber-500 cursor-pointer" onClick={() => setTheme('light')} />
-                : <BsMoonFill className="text-xl hover:text-amber-500 cursor-pointer" onClick={() => setTheme('dark')} />
-            }
-        </div>
-    )
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  const currentTheme = theme == "system" ? systemTheme : theme;
+  return (
+    <div>
+      {mounted && theme == "dark" ? (
+        <MdLightMode
+          className="text-xl hover:text-amber-500 cursor-pointer"
+          onClick={() => setTheme("light")}
+        />
+      ) : (
+        <BsMoonFill
+          className="text-xl hover:text-amber-500 cursor-pointer"
+          onClick={() => setTheme("dark")}
+        />
+      )}
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function Home({children}) {
-  return (
-    <div>Home</div>
-  )
+export default function Home({ children }) {
+  return <div>Home</div>;
 }
