4d7945c9afb440d6c9a9002d8df0d47013ded530
Muneeb Rehman <117652966+MuneebRehman8@users.noreply.github.com>
2024-01-17 20:44:46 +0500

    Add feching functionalities

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,22 @@
 import Image from "next/image";
+import Results from "./components/results";
+const API_KEY = process.env.API_KEY;
 
-export default function Home({ children }) {
-  return <div>Home</div>;
+export default async function Home({ searchParams }) {
+  const url_opt =
+    searchParams?.genre == "fetch_top_rated"
+      ? "movie/top_rated"
+      : "trending/all/week";
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${url_opt}?api_key=${API_KEY}&language=en-US`,
+    { revalidate: 10000 }
+  );
+  const data = await response.json();
+
+  return (
+    <div>
+      <Results results={data.results} />
+    </div>
+  );
 }
